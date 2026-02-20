70559cbda1321794958caf2710535ea778ccb7c0
Jhennifer <jhenniferfernandasantosmarques@gmail.com>
2026-02-19 22:08:06 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function toggleMode(){
+  const html = document.documentElement
+
+  /* if(html.classList.contains('light')){
+    html.classList.remove('light')
+  }else{
+    html.classList.add('light')
+  } */
+
+  html.classList.toggle(
+    "light",
+  ) /* essa linha faz a mesma coisa que o código comentado acima, através da função toggle */
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo metade roxo metade azul",
+    )
+  } else {
+    //se tiver sem light mode,manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo",
+    )
+  }
+
+ 
+ 
+}
